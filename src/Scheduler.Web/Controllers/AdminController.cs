2323ca08751078e671b41868424a93b422db0423
Noah Woyak <17170815@mstc.edu>
2023-02-18 15:10:40 -0600

    Start on admin dashboard

---
+++ src/Scheduler.Web/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scheduler.Web.Controllers;
+
+[Authorize]
+public sealed class AdminController : Controller
+{
+	/// <summary>
+	/// Displays the Admin Dashboard view.
+	/// </summary>
+	/// <returns>A dashboard where admins can manage fields, teams, events, etc.</returns>
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
