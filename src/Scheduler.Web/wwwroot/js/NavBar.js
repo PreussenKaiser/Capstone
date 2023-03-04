3654553f91d624f048d9ba9406f302fc3e8e1434
Bart Downey <13835249@mstc.edu>
2023-03-03 21:33:35 -0600

    Adjusting Navbar buttons, and making it sticky

---
+++ src/Scheduler.Web/wwwroot/js/NavBar.js
@@ -0,0 +1,13 @@
+﻿window.onscroll = function () { fixToTop() };
+
+var navbar = document.getElementById("topNav");
+
+var sticky = navbar.offsetTop;
+
+function fixToTop() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("nav-fixed")
+    } else {
+        navbar.classList.remove("nav-fixed");
+    }
+}
