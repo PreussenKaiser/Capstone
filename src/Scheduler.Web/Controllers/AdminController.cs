425a6852af236d97e240c09460ddbc249aacf981
Noah Woyak <17170815@mstc.edu>
2023-03-05 15:12:31 -0600

    Add tables to admin dashboard

--- src/Scheduler.Web/Controllers/AdminController.cs
+++ src/Scheduler.Web/Controllers/AdminController.cs
@@ -1,17 +1,50 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Scheduler.Core.Models.Identity;
+using Scheduler.Core.Services;
+using Scheduler.Web.ViewModels;
 
 namespace Scheduler.Web.Controllers;
 
-[Authorize]
+[Authorize(Roles ="Admin")]
 public sealed class AdminController : Controller
 {
+
+	private readonly IScheduleService scheduleService;
+
+	private readonly IFieldService fieldService;
+
+	private readonly ITeamService teamService;
+
+	private readonly UserManager<User> userManager;
+
+	public AdminController(IScheduleService scheduleService, IFieldService fieldService, ITeamService teamService, UserManager<User> userManager) {
+		this.scheduleService = scheduleService;
+		this.fieldService = fieldService;
+		this.teamService = teamService;
+		this.userManager = userManager;
+	}
+
 	/// <summary>
 	/// Displays the Admin Dashboard view.
 	/// </summary>
-	/// <returns>A dashboard where admins can manage fields, teams, events, etc.</returns>
-	public IActionResult Index()
+	/// <returns>A dashboard where admins can manage fields, teams, events and users.</returns>
+	public async Task<IActionResult> Index()
 	{
-		return View();
+		var events = await scheduleService.GetAllAsync();
+		var fields = await fieldService.GetAllAsync();
+		var teams = await teamService.GetAllAsync();
+		var users = userManager.Users;
+
+		AdminDashboardViewModel viewModel = new AdminDashboardViewModel
+		{
+			Events = events.ToList(),
+			Fields = fields.ToList(),
+			Teams = teams.ToList(),
+			Users = users.ToList()
+		};
+
+		return View(viewModel);
 	}
 }
---
+++ src/Scheduler.Web/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Scheduler.Core.Models;
+using Scheduler.Core.Models.Identity;
+
+namespace Scheduler.Web.ViewModels;
+
+public sealed record AdminDashboardViewModel
+{
+	public List<Event>? Events { get; set; }
+
+	public List<Field>? Fields { get; set; }
+
+	public List<Team>? Teams { get; set; }
+
+	public List<User>? Users { get; set; }
+}
