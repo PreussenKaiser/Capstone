b1de55ec92d83bc931d5e89c88b4026e35b32a2e
Karl Lukan <klukan20@gmail.com>
2023-03-26 22:14:15 -0500

    fixing requireiffalse

--- src/Scheduler/Domain/Validation/RequiredIfFalseAttribute.cs
+++ src/Scheduler/Domain/Validation/RequiredIfFalseAttribute.cs
@@ -5,25 +5,25 @@ namespace Scheduler.Domain.Validation;
 /// <summary>
 /// Checks if a seperate property is false before determining if the field is required.
 /// </summary>
-public sealed class RequiredIfFalseAttribute: RequiredAttribute
+public sealed class RequiredIfFalseAttribute : RequiredAttribute
 {
 	private string PropertyName { get; set; }
 
 	public RequiredIfFalseAttribute(string propertyName)
 	{
-		PropertyName = propertyName;
+		this.PropertyName = propertyName;
 	}
 
-	protected override ValidationResult IsValid(object value, ValidationContext context)
+	protected override ValidationResult? IsValid(object? value, ValidationContext context)
 	{
 		object instance = context.ObjectInstance;
 		Type type = instance.GetType();
 
-		bool.TryParse(type.GetProperty(PropertyName).GetValue(instance)?.ToString(), out bool propertyValue);
+		bool.TryParse(type.GetProperty(this.PropertyName).GetValue(instance)?.ToString(), out bool propertyValue);
 
 		if (!propertyValue && string.IsNullOrWhiteSpace(value?.ToString()))
 		{
-			return new ValidationResult(ErrorMessage);
+			return new ValidationResult(base.ErrorMessage);
 		}
 
 		return ValidationResult.Success;
