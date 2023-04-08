116c24661334c801eaabb3ff4c7575875679cdd0
Brolinski <78505040+Brolinski@users.noreply.github.com>
2023-04-08 03:21:01 -0500

    Fixed the data being stored.

--- src/Scheduler/wwwroot/js/custom-minutes-dropdown.js
+++ src/Scheduler/wwwroot/js/custom-minutes-dropdown.js
@@ -1,18 +1,2 @@
-$(function () {
-    $('#StartDate').daterangepicker({
-        timePicker: true,
-        startDate: moment().startOf('hour'),
-        endDate: moment().startOf('hour').add(32, 'hour'),
-        timePickerIncrement: 30,
-        locale: {
-            format: 'M/DD hh:mm A'
-        }
-    }).on('apply.daterangepicker', function (ev, picker) {
-        var startDate = picker.startDate.format('M/DD/YYYY hh:mm A');
-        var endDate = picker.endDate.format('M/DD/YYYY hh:mm A');
 
-        $('#StartDate').val(startDate);
-        $('#EndDate').val(endDate);
-    });
-});
 
