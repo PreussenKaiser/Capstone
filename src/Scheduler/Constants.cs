168cc2b03bb76ef8a7f5ca66eef90cd55034bb12
Karl Lukan <klukan20@gmail.com>
2023-05-15 19:13:02 -0500

    saving

--- src/Scheduler/Constants.cs
+++ src/Scheduler/Constants.cs
@@ -1,8 +1,17 @@
 ﻿namespace Scheduler;
 
-public static class Constants
+/// <summary>
+/// Application constants.
+/// </summary>
+internal static class Constants
 {
+	/// <summary>
+	/// Standard application date format.
+	/// </summary>
 	public const string DATE_FORMAT = "yyyy-MM-dd";
 
+	/// <summary>
+	/// Date format for the date picker.
+	/// </summary>
 	public const string DATETIME_FORMAT = "yyyy-MM-dd HH:mm";
 }
